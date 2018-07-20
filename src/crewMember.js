e558e3d5a016892b780a6aa1e25ccd016689cbde
QMaximillian <quinnlashinsky@gmail.com>
2018-07-20 16:23:27 -0400

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,33 @@
-class CrewMember {}
+class CrewMember {
+
+	constructor(position) {
+		this.position = position
+		this.currentShip = 'Looking for a Rig'
+	}
+
+	chargePhasers(){
+		if(this.position === 'Gunner' && typeof this.currentShip === Spaceship){
+			this.currentShip.phasersCharge = 'charged'
+		} else {
+			return 'had no effect'
+		}
+	}
+
+	engageWarpDrive(){
+		if(this.position === 'Pilot' && typeof this.currentShip === Spaceship){
+			this.currentShip.warpDrive = 'engaged'
+		} else {
+			return 'had no effect'
+		}
+	}
+
+	setsInvisibility(){
+		debugger
+		if(this.position === 'Defender' && this.currentShip !== 'Looking for a Rig'){
+			this.currentShip.cloaked = true
+		} else {
+			return 'had no effect'
+		}
+	}
+
+}
--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,19 @@
-class Spaceship {}
+class Spaceship {
+
+	constructor(name, crew, phasers, shields) {
+		this.name = name
+		this.crew = crew
+		this.phasers = phasers
+		this.shields = shields
+		this.cloaked = false
+		this.warpDrive = 'disengaged'
+		this.phasersCharge = 'uncharged'
+		this.docked = this.crew.length === 0 ? true : false
+
+		for(let el of this.crew){
+			el.currentShip = this
+		}
+
+	}
+
+}
