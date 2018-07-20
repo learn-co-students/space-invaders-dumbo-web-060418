315b7883489c194644eeeb21ea4b94fbacf20a56
Indira Williams <indirajamurath@gmail.com>
2018-07-20 17:37:47 -0400

    work in progress

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,28 @@
-class CrewMember {}
+class CrewMember {
+  constructor(position){
+    this.position = position;
+    this.currentShip = 'Looking for a Rig'
+  }
+
+  engageWarpDrive(){
+    if (this.currentShip === 'Looking for a Rig'){
+      return 'had no effect'
+    }
+  }
+
+  setsInvisibility(){
+    if (this.currentShip === 'Looking for a Rig'){
+      return 'had no effect'
+    }
+    else {
+    }
+  }
+
+  chargePhasers(){
+    if (this.currentShip === 'Looking for a Rig'){
+      return 'had no effect'
+    }
+  }
+
+
+}
--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,20 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, CrewMembers, phasers, shields){
+    this.name = name
+    this.CrewMembers = CrewMembers
+    this.phasers = phasers
+    this.shields = shields
+    this.cloaked = false
+    this.docked = true
+    this.warpDrive = 'disengaged'
+    this.phasersCharge = 'uncharged'
+  }
+
+  assignCrewToShip() {
+    this.CrewMembers.forEach(function(member){
+      member.currentShip = this
+    })
+    this.docked = false
+  }
+
+}
